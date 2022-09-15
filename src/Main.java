927c48f3952f4ba03de8fe20e40eb201a54b2505
Colton Fox <coltonfox@coltons-mbp.wifi.uc.edu>
2022-09-15 15:11:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int springCost = 80;
+        int summerCost = 100;
+        int fallCost = 70;
+        int winterCost = 100;
+
+        int totalCost = springCost + summerCost + fallCost + winterCost;
+           System.out.println("Your yearly maintenance cost is " + totalCost + " dollars ");
+
+    }
+}
